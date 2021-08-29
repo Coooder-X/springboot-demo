e7cc36d7b820b1dbc58477e0899b03d5d7f91b20
Coooder-X <1104143015@qq.com>
2021-08-29 17:42:30 +0800

    前端组件布局初步

---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <el-container>
+      <el-header style="height: 50px;">
+        <Header></Header>
+      </el-header>
+      <el-container style="height: calc(100vh - 50px);">
+        <el-aside style="width: 250px; background-color: antiquewhite">
+          <Aside></Aside>
+        </el-aside>
+        <el-main>
+          <Main></Main>
+        </el-main>
+      </el-container>
+<!--      <div style="display: flex;">-->
+<!--        -->
+<!--        <router-view style="flex: 1; background-color: #cccccc"></router-view>-->
+<!--      </div>-->
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Aside from "./Aside";
+import Main from "./Main";
+export default {
+  name: 'Home',
+  components: {Aside, Header, Main},
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ front-end/src/components/Main.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+<!--    button block-->
+    <div style="margin-bottom: 10px;">
+      <el-button size="small" type="primary">新增</el-button>
+      <el-button size="small" type="success">导入</el-button>
+      <el-button size="small" type="warning">导出</el-button>
+    </div>
+<!--    search block-->
+    <div style="margin-bottom: 15px;">
+      <el-input v-model="search" placeholder="please input" style="width: 20%;"></el-input>
+      <el-button type="info">GO</el-button>
+    </div>
+    <el-table
+      :data="tableData"
+      stripe
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
+          <el-popconfirm title="确定删除吗？">
+            <el-button size="small" type="text" slot="reference">删除</el-button>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="margin: 10px 0px;">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[5, 10, 20]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Main",
+        data() {
+            return {
+                search: '',
+                currentPage: 1,
+                total: 20,
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
+            }
+        },
+        methods: {
+            handleClick(row) {
+                console.log(row);
+            },
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
